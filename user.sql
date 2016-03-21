32d06efdd8fcb5433b86415d07d78a5710ab74f1
og39 <og39@njit.edu>
2016-03-21 17:25:18 -0400

    Added create database to .sql

--- user.sql
+++ user.sql
@@ -1,4 +1,5 @@
-
+drop database if exists Workouts;
+create database Workouts;
 create user 'WorkoutAdmin'@'localhost' identified by "midterm";
 grant all privileges on Workouts.* to 'WorkoutAdmin'@'localhost';
 flush privileges;
